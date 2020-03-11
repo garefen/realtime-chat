7f467ffd162db275388d3e8a43b41dd443e42cf2
Gabriel Sarti <gabriel.sarti@rga.com>
2020-03-11 15:35:44 -0300

    Updates

---
+++ precache-manifest.1346ab32c4b40a8954f5f272aa863f88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e117fa4da1767d793086c0a7c0eb993a",
+    "url": "/realtime-chat/index.html"
+  },
+  {
+    "revision": "09cfc54be851a054a0b1",
+    "url": "/realtime-chat/static/css/main.ada0b06a.chunk.css"
+  },
+  {
+    "revision": "f089f556a285678af931",
+    "url": "/realtime-chat/static/js/2.44ef56a0.chunk.js"
+  },
+  {
+    "revision": "07a707eba59e3e3ed782f77b31f34780",
+    "url": "/realtime-chat/static/js/2.44ef56a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cfc54be851a054a0b1",
+    "url": "/realtime-chat/static/js/main.4b51b8a1.chunk.js"
+  },
+  {
+    "revision": "2ac03044323ba6910243",
+    "url": "/realtime-chat/static/js/runtime-main.2dcd4f8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/realtime-chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
